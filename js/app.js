6174ad98af10f3d6ff6e6d1b8b6917298a41e330
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-02-27 10:33:12 +0000

    created class and event listeners

--- js/app.js
+++ js/app.js
@@ -15,4 +15,41 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
   }
+  ///submit budget method
+  submitBudgetForm(){
+    
+  }
+
+}
+function eventListeners(){
+const budgetForm=document.getElementById('budget-form');
+const expenseForm=expenseForm = document.getElementById("expense-form");
+const expenseList=expenseForm = document.getElementById("expense-list");
+
+///new instance of Ui Class 
+const ui= new UI();
+
+
+//budget Form submit
+budgetForm.addEventListener('submit',function(event){
+event.preventDefault()
+
+})
+//expense Form submit
+expenseForm.addEventListener('submit',function(event){
+  event.preventDefault()
+
+
+})
+//expense click
+expenseList.addEventListener('click',function(){
+  
+
+})
+
+
+document.addEventListener('DOMContentLoaded',function(){
+  eventListeners();
+})
+
 }
